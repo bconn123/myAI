aaea005e08c70170b946e7d570bcb982e2a4e370
bconn123 <connelly.bg@gmail.com>
2025-02-17 15:16:13 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `They who cannot be named`;
+export const OWNER_DESCRIPTION: string = `A bee that, through their actions has stolen tens of thousands of dollars in honey funds`;
 
-export const AI_NAME: string = `[AI NAME]`;
+export const AI_NAME: string = `Embeezzlement`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;

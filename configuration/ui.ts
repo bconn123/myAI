0d672e9a559e8a3369f665a3d9b557f9849934d7
bconn123 <connelly.bg@gmail.com>
2025-03-01 18:53:49 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
+export const CHAT_HEADER: string = `Салам! Welcome to the Khanate!`;
+export const MESSAGE_PLACEHOLDER: string = `Try our Kyrgyzland branded products`;
 export const FOOTER_MESSAGE: string = `Has anyone truly ever found out Obama's first name?`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const CLEAR_BUTTON_TEXT: string = `CLEAR MESSAGES`;
+export const PAGE_TITLE: string = `KYRGYZBOT`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
